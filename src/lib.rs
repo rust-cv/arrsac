2834d149c9aefcb293826cd60283041014ecc89d
Geordon Worley <vadixidav@gmail.com>
2019-10-12 19:16:02 -0400

    add likelyhood_ratio function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(type_alias_impl_trait)]
 
-use sample_consensus::{Consensus, Estimator, Model};
+use sample_consensus::{Consensus, Estimator, EstimatorData, Model};
 
 struct Arrsac {
     max_candidate_hypothesis: usize,
@@ -14,18 +14,11 @@ where
     type Inliers = Inliers;
     type ModelInliers = impl Iterator<Item = (E::Model, Self::Inliers)>;
 
-    fn model<I>(&mut self, estimator: &E, data: I) -> Option<E::Model>
-    where
-        I: Iterator<Item = <<E as Estimator>::Model as Model>::Data> + Clone,
-    {
+    fn model(&mut self, estimator: &E, data: &[EstimatorData<E>]) -> Option<E::Model> {
         unimplemented!()
     }
 
-    fn models<I>(&mut self, estimator: &E, data: I) -> Self::ModelInliers
-    where
-        I: Iterator<Item = <<E as Estimator>::Model as Model>::Data> + Clone,
-    {
-        unimplemented!();
+    fn models(&mut self, estimator: &E, data: &[EstimatorData<E>]) -> Self::ModelInliers {
         vec![].into_iter()
     }
 }
@@ -38,4 +31,30 @@ impl Iterator for Inliers {
     fn next(&mut self) -> Option<usize> {
         unimplemented!()
     }
+}
+
+/// Algorithm 1 in "Randomized RANSAC with Sequential Probability Ratio Test".
+///
+/// Items with a residual error less than `threshold` are considered inliers.
+///
+/// `positive_likelyhood` - `δ / ε`
+/// `negative_likelyhood` - `(1 - δ) / (1 - ε)`
+fn likelyhood_ratio<'a, M: Model>(
+    data: impl Iterator<Item = &'a M::Data>,
+    model: &M,
+    threshold: f32,
+    positive_likelyhood: f32,
+    negative_likelyhood: f32,
+) -> f32
+where
+    M::Data: 'a,
+{
+    data.map(|data| {
+        if model.residual(data) < threshold {
+            positive_likelyhood
+        } else {
+            negative_likelyhood
+        }
+    })
+    .product()
 }
