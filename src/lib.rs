943b8339180439c6fb10c2ea9e5e036f189fa795
Geordon Worley <vadixidav@gmail.com>
2021-09-02 17:55:22 -0500

    correct spelling of likelyhood to likelihood

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ pub struct Arrsac<R> {
     max_candidate_hypotheses: usize,
     block_size: usize,
     max_delta_estimations: usize,
-    likelyhood_ratio_threshold: f32,
+    likelihood_ratio_threshold: f32,
     initial_epsilon: f32,
     initial_delta: f32,
     inlier_threshold: f64,
@@ -35,14 +35,14 @@ where
     /// The `inlier_threshold` is the one parameter that is always specific to your dataset.
     /// This must be set to the threshold in which a data point's residual is considered an inlier.
     /// Some of the other parameters may need to be configured based on the amount of data,
-    /// such as `block_size`, `likelyhood_ratio_threshold`, and `block_size`. However,
+    /// such as `block_size`, `likelihood_ratio_threshold`, and `block_size`. However,
     /// `inlier_threshold` has to be set based on the residual function used with the model.
     pub fn new(inlier_threshold: f64, rng: R) -> Self {
         Self {
             max_candidate_hypotheses: 50,
             block_size: 100,
             max_delta_estimations: 4,
-            likelyhood_ratio_threshold: 1e6,
+            likelihood_ratio_threshold: 1e6,
             initial_epsilon: 0.1,
             initial_delta: 0.05,
             inlier_threshold,
@@ -79,7 +79,7 @@ where
         }
     }
 
-    /// Once a model reaches this level of unlikelyhood, it is rejected. Set this
+    /// Once a model reaches this level of unlikelihood, it is rejected. Set this
     /// higher to make it less restrictive, usually at the cost of more execution time.
     ///
     /// Increasing this will make it more likely to find a good result (unless it is set very high).
@@ -92,9 +92,9 @@ where
     /// that a good model is rejected.
     ///
     /// Default: `1e6`
-    pub fn likelyhood_ratio_threshold(self, likelyhood_ratio_threshold: f32) -> Self {
+    pub fn likelihood_ratio_threshold(self, likelihood_ratio_threshold: f32) -> Self {
         Self {
-            likelyhood_ratio_threshold,
+            likelihood_ratio_threshold,
             ..self
         }
     }
@@ -159,8 +159,8 @@ where
         let mut epsilon = self.initial_epsilon;
         // Set the initial delta (outlier ratio in good model).
         let mut delta = self.initial_delta;
-        let mut positive_likelyhood_ratio = delta / epsilon;
-        let mut negative_likelyhood_ratio = (1.0 - delta) / (1.0 - epsilon);
+        let mut positive_likelihood_ratio = delta / epsilon;
+        let mut negative_likelihood_ratio = (1.0 - delta) / (1.0 - epsilon);
         let mut current_delta_estimations = 0;
         let mut total_delta_inliers = 0;
         let mut best_inlier_indices = vec![];
@@ -186,8 +186,8 @@ where
                 let (pass, inliers) = self.asprt(
                     data.clone().take(initial_datapoints),
                     &model,
-                    positive_likelyhood_ratio,
-                    negative_likelyhood_ratio,
+                    positive_likelihood_ratio,
+                    negative_likelihood_ratio,
                     E::MIN_SAMPLES,
                 );
                 if pass {
@@ -197,10 +197,10 @@ where
                         best_inliers = inliers;
                         // Update epsilon (this can only increase, since there are more inliers).
                         epsilon = inliers as f32 / initial_datapoints as f32;
-                        // Will decrease positive likelyhood ratio.
-                        positive_likelyhood_ratio = delta / epsilon;
-                        // Will increase negative likelyhood ratio.
-                        negative_likelyhood_ratio = (1.0 - delta) / (1.0 - epsilon);
+                        // Will decrease positive likelihood ratio.
+                        positive_likelihood_ratio = delta / epsilon;
+                        // Will increase negative likelihood ratio.
+                        negative_likelihood_ratio = (1.0 - delta) / (1.0 - epsilon);
 
                         // Update the inlier indices appropriately.
                         best_inlier_indices = self.inliers(data.clone(), &model);
@@ -216,10 +216,10 @@ where
                     // Update delta.
                     delta = total_delta_inliers as f32
                         / (current_delta_estimations * data.clone().count()) as f32;
-                    // May change positive likelyhood ratio.
-                    positive_likelyhood_ratio = delta / epsilon;
-                    // May change negative likelyhood ratio.
-                    negative_likelyhood_ratio = (1.0 - delta) / (1.0 - epsilon);
+                    // May change positive likelihood ratio.
+                    positive_likelihood_ratio = delta / epsilon;
+                    // May change negative likelihood ratio.
+                    negative_likelihood_ratio = (1.0 - delta) / (1.0 - epsilon);
                 }
             }
         }
@@ -291,27 +291,27 @@ where
     /// This tests if a model is accepted. Returns `true` on accepted and `false` on rejected.
     ///
     /// `inlier_threshold` - The model residual error threshold between inliers and outliers
-    /// `positive_likelyhood_ratio` - `δ / ε`
-    /// `negative_likelyhood_ratio` - `(1 - δ) / (1 - ε)`
+    /// `positive_likelihood_ratio` - `δ / ε`
+    /// `negative_likelihood_ratio` - `(1 - δ) / (1 - ε)`
     fn asprt<Data, M: Model<Data>>(
         &self,
         data: impl Iterator<Item = Data>,
         model: &M,
-        positive_likelyhood_ratio: f32,
-        negative_likelyhood_ratio: f32,
+        positive_likelihood_ratio: f32,
+        negative_likelihood_ratio: f32,
         minimum_samples: usize,
     ) -> (bool, usize) {
-        let mut likelyhood_ratio = 1.0;
+        let mut likelihood_ratio = 1.0;
         let mut inliers = 0;
         for data in data {
-            likelyhood_ratio *= if model.residual(&data) < self.inlier_threshold {
+            likelihood_ratio *= if model.residual(&data) < self.inlier_threshold {
                 inliers += 1;
-                positive_likelyhood_ratio
+                positive_likelihood_ratio
             } else {
-                negative_likelyhood_ratio
+                negative_likelihood_ratio
             };
 
-            if likelyhood_ratio > self.likelyhood_ratio_threshold {
+            if likelihood_ratio > self.likelihood_ratio_threshold {
                 return (false, 0);
             }
         }
@@ -405,9 +405,9 @@ where
             // Technically model 0 might no longer be the best model after evaluating the last data-point,
             // but that is not that important.
             let epsilon = hypotheses[0].1 as f32 / samples_up_to_end_of_block as f32;
-            // Create the likelyhood ratios for inliers and outliers.
-            let positive_likelyhood_ratio = delta / epsilon;
-            let negative_likelyhood_ratio = (1.0 - delta) / (1.0 - epsilon);
+            // Create the likelihood ratios for inliers and outliers.
+            let positive_likelihood_ratio = delta / epsilon;
+            let negative_likelihood_ratio = (1.0 - delta) / (1.0 - epsilon);
             // Generate the list of inliers for the best model.
             let inliers = self.inliers(data.clone(), &hypotheses[0].0);
             // We generate hypotheses until we reach the initial num hypotheses.
@@ -423,8 +423,8 @@ where
                     let (pass, inliers) = self.asprt(
                         data.clone().take(samples_up_to_end_of_block),
                         &model,
-                        positive_likelyhood_ratio,
-                        negative_likelyhood_ratio,
+                        positive_likelihood_ratio,
+                        negative_likelihood_ratio,
                         E::MIN_SAMPLES,
                     );
                     if pass {
